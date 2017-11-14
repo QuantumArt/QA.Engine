2b8067c6cf162eddc944479159ad56809fb82ec5
Paul Grigorenko <grigorenko.paul@gmail.com>
2017-11-14 16:28:34 +0300

    add current page id to onscreen viewcomponent

--- QA.DotNetCore.Engine/src/QA.DotNetCore.Engine.Widgets/OnScreen/OnScreenViewComponent.cs
+++ QA.DotNetCore.Engine/src/QA.DotNetCore.Engine.Widgets/OnScreen/OnScreenViewComponent.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc;
+using QA.DotNetCore.Engine.Routing;
 using QA.DotNetCore.Engine.Widgets.Configuration;
 
 namespace QA.DotNetCore.Engine.Widgets.OnScreen
@@ -16,8 +17,9 @@ namespace QA.DotNetCore.Engine.Widgets.OnScreen
         public HtmlString Invoke()
         {
             var enabled = ViewContext.HttpContext.OnScreenEditEnabled();
+            var ai = ViewContext.GetCurrentItem();
             if (enabled)
-                return new HtmlString($@"<div id='sidebarplaceholder'></div><script type='text/javascript'>window.onScreenAdminBaseUrl = '{_onScreenSettings.AdminSiteBaseUrl}';</script><script src='{ _onScreenSettings.AdminSiteBaseUrl}/dist/onScreenLoader.js' defer></script>");
+                return new HtmlString($@"<div id='sidebarplaceholder'></div><script type='text/javascript'>window.onScreenAdminBaseUrl = '{_onScreenSettings.AdminSiteBaseUrl}';window.currentPageId='{ai?.Id}';</script><script src='{ _onScreenSettings.AdminSiteBaseUrl}/dist/onScreenLoader.js' defer></script>");
             return HtmlString.Empty;
         }
     }

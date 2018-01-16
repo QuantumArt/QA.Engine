96f4d4fcd7febce9974226b1a8ffabfe6cf29cbf
Mikhail <vozzic@gmail.com>
2018-01-16 21:27:05 +0300

    tests states + ui improvments

--- QA.DotNetCore.Engine/src/QA.DotNetCore.OnScreenAdmin.Web/ClientApp/containers/AbTestingScreen/index.js
+++ QA.DotNetCore.Engine/src/QA.DotNetCore.OnScreenAdmin.Web/ClientApp/containers/AbTestingScreen/index.js
@@ -1,9 +1,21 @@
 import { connect } from 'react-redux';
 import AbtestingScreen from 'Components/AbTestingScreen';
 
-const mapStateToProps = ({ abTestingScreen: { avalaibleTests, testsData } }) => ({
-  tests: avalaibleTests.map((el, i) => ({ ...el, ...testsData[i] })),
-});
+const mapStateToProps = ({ abTestingScreen }) => {
+  const { avalaibleTests, testsData } = abTestingScreen;
+  const combined = avalaibleTests.map((el, i) => {
+    const states = {
+      globalActive: el.choice !== null && testsData[i].enabled,
+      sessionActive: el.choice !== null && !testsData[i].enabled,
+      paused: el.choice === null && testsData[i].enabled,
+      stoped: el.choice === null && !testsData[i].enabled,
+    };
+
+    return { ...el, ...testsData[i], ...states };
+  });
+
+  return { tests: combined };
+};
 
 const AbtestingScreenContainer = connect(
   mapStateToProps,

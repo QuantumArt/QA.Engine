59e0b7cc811bbcd7dd8bdc2100c5307e5a888ebe
Paul Grigorenko <grigorenko.paul@gmail.com>
2018-06-20 16:18:35 +0300

    1) поправил конфигурирование сайта в startup.cs и program.cs (сделал в духе aspnet core 2) 2) удалил лишние ссылки на пакеты

--- QA.DotNetCore.Engine/samples/QA.DemoSite/QA.DemoSite/Program.cs
+++ QA.DotNetCore.Engine/samples/QA.DemoSite/QA.DemoSite/Program.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
+using System;
 
 namespace QA.DemoSite
 {
@@ -15,25 +10,35 @@ namespace QA.DemoSite
     {
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
-                    .AddCommandLine(args)
-                    .AddEnvironmentVariables(prefix: "ASPNETCORE_")
-                    .Build();
-
-            var host = new WebHostBuilder()
-                .UseConfiguration(config)
-               .UseKestrel()
-               .UseContentRoot(Directory.GetCurrentDirectory())
-               .UseIISIntegration()
-               .UseStartup<Startup>()
-               .Build();
-
-            host.Run();
+            // NLog: setup the logger first to catch all errors
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main");
+                BuildWebHost(args).Run();
+            }
+            catch (Exception ex)
+            {
+                //NLog: catch setup errors
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog()  // NLog: setup NLog for Dependency injection
                 .Build();
     }
 }
--- QA.DotNetCore.Engine/samples/QA.DemoSite/QA.DemoSite/Startup.cs
+++ QA.DotNetCore.Engine/samples/QA.DemoSite/QA.DemoSite/Startup.cs
@@ -1,45 +1,31 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using QA.DemoSite.Models.Pages;
+using QA.DotNetCore.Engine.Abstractions;
+using QA.DotNetCore.Engine.QpData.Configuration;
 using QA.DotNetCore.Engine.QpData.Settings;
 using QA.DotNetCore.Engine.Routing.Configuration;
-using QA.DotNetCore.Engine.QpData.Configuration;
-using QA.DotNetCore.Engine.Abstractions;
-using NLog.Web;
-using NLog.Extensions.Logging;
-using QA.DemoSite.Models.Pages;
-using QA.DotNetCore.Engine.QpData;
 using System.Net;
-using Microsoft.AspNetCore.Diagnostics;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Routing;
 
 namespace QA.DemoSite
 {
     public class Startup
     {
-        private IHostingEnvironment _env;
-
-        public Startup(IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-            _env = env;
-            env.ConfigureNLog("nlog.config");
+            Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddMvc();// .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddLogging();
             services.AddSingleton<ILogger>(provider => provider.GetRequiredService<ILogger<Program>>());
@@ -54,11 +40,9 @@ namespace QA.DemoSite
                 options.QpSiteStructureSettings = Configuration.GetSection("QpSiteStructureSettings").Get<QpSiteStructureSettings>();
                 options.TypeFinder.RegisterFromAssemblyContaining<RootPage, IAbstractItem>();
             });
-
-            services.AddSingleton<IAbstractItemStorageProvider, SimpleAbstractItemStorageProvider>();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger logger)
         {
             if (env.IsDevelopment())
             {
@@ -68,9 +52,6 @@ namespace QA.DemoSite
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-
-            loggerFactory.AddNLog();
-            var logger = loggerFactory.CreateLogger(typeof(Startup));
 
             app.UseExceptionHandler(options =>
             {
@@ -94,9 +75,6 @@ namespace QA.DemoSite
 
             app.UseMvc(routes =>
             {
-                routes.MapContentRoute("Route with custom params", "{controller}/{id}/{page}",
-                    defaults: new RouteValueDictionary(new { action = "details" }));
-
                 routes.MapContentRoute("default", "{controller}/{action=Index}/{id?}");
                 routes.MapRoute("static controllers route", "{controller}/{action=Index}/{id?}");
             });

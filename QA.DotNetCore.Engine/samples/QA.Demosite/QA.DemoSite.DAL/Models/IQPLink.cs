d4d589d64a1e054c4a9c4d65ca405513a06f9fdb
Anastasya Kuznetsova <KuznetsovaA@quantumart.ru>
2019-04-05 16:21:14 +0300

    install ef core to demosite

---
+++ QA.DotNetCore.Engine/samples/QA.Demosite/QA.DemoSite.DAL/Models/IQPLink.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+
+namespace Quantumart.QP8.EntityFrameworkCore
+{
+    public interface IQPLink
+    {
+        int Id { get; set; }
+        int LinkedItemId { get; set; }
+        int LinkId { get; }
+
+        IQPArticle Item { get; }
+        IQPArticle LinkedItem { get; }
+    }
+
+}

71043cf7b91c1fed15c028bfcb19b6decc46d9ef
Paul Grigorenko <grigorenko.paul@gmail.com>
2019-09-16 21:50:20 +0300

    поправил тесты

--- QA.DotNetCore.Engine/src/QA.DotNetCore.Engine.Persistent.Dapper.Tests/AbstractItemRepositoryTests.cs
+++ QA.DotNetCore.Engine/src/QA.DotNetCore.Engine.Persistent.Dapper.Tests/AbstractItemRepositoryTests.cs
@@ -3,8 +3,10 @@ using QA.DotNetCore.Engine.QpData.Persistent.Dapper;
 
 namespace Tests
 {
-    public class Tests
+    public class AbstractItemRepositoryTests
     {
+        private MetaInfoRepository _sqlMetaRepo;
+        private MetaInfoRepository _postgresMetaRepo;
         private AbstractItemRepository _sqlRepository;
         private AbstractItemRepository _postgresRepository;
         private UnitOfWork _sqlUnitOfWork;
@@ -16,15 +18,14 @@ namespace Tests
             _sqlUnitOfWork =
                 new UnitOfWork(
                     "Initial Catalog=qa_demosite;Data Source=spbdevsql01\\dev;User ID=publishing;Password=QuantumartHost.SQL");
-            var sqlMetaRepo = new MetaInfoRepository(_sqlUnitOfWork);
-            var sqlAnalyzer = new NetNameQueryAnalyzer(sqlMetaRepo);
-            _sqlRepository = new AbstractItemRepository(_sqlUnitOfWork
-                , sqlAnalyzer);
+            _sqlMetaRepo = new MetaInfoRepository(_sqlUnitOfWork);
+            var sqlAnalyzer = new NetNameQueryAnalyzer(_sqlMetaRepo);
+            _sqlRepository = new AbstractItemRepository(_sqlUnitOfWork, sqlAnalyzer, _postgresMetaRepo);
             _postgresUnitOfWork =
                 new UnitOfWork("Server=mscpgsql01;Port=5432;Database=qa_demosite;User Id=postgres;Password=1q2w-p=[;", "pg");
-            var postgresMetaRepo = new MetaInfoRepository(_postgresUnitOfWork);
-            var postgresAnalyzer = new NetNameQueryAnalyzer(postgresMetaRepo);
-            _postgresRepository = new AbstractItemRepository(_postgresUnitOfWork, postgresAnalyzer);
+            _postgresMetaRepo = new MetaInfoRepository(_postgresUnitOfWork);
+            var postgresAnalyzer = new NetNameQueryAnalyzer(_postgresMetaRepo);
+            _postgresRepository = new AbstractItemRepository(_postgresUnitOfWork, postgresAnalyzer, _postgresMetaRepo);
         }
 
         [Test]
@@ -32,8 +33,17 @@ namespace Tests
         {
             Assert.DoesNotThrow(() =>
             {
-                _sqlRepository.GetAbstractItemExtensionData(30751, new[] {741180, 741278}, true, false);
-                _postgresRepository.GetAbstractItemExtensionData(30751, new[] {741180, 741278}, true, false);
+                var startPageId = 741114;
+
+                var sqlBaseContent = _sqlMetaRepo.GetContent("QPAbstractItem", 52);//получим инфу о контенте AbstractItem
+                var sqlExtData = _sqlRepository.GetAbstractItemExtensionData(547, new[] { startPageId }, sqlBaseContent, true, false);//получим данные о extension c типом StartPageExtension с id=startPageId
+                Assert.True(sqlExtData.ContainsKey(startPageId));
+                Assert.IsNotNull(sqlExtData[startPageId].Get("Bindings", typeof(string)));
+                
+                var pgBaseContent = _postgresMetaRepo.GetContent("QPAbstractItem", 52);
+                var pgExtData = _postgresRepository.GetAbstractItemExtensionData(547, new[] { startPageId }, pgBaseContent, true, false);
+                Assert.True(pgExtData.ContainsKey(startPageId));
+                Assert.IsNotNull(pgExtData[startPageId].Get("Bindings", typeof(string)));
             });
         }
 

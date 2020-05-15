87daa54036e520f352ad42ffe3f0694eb4da587a
Paul Grigorenko <paull@inbox.ru>
2020-05-15 18:29:18 +0300

    добавил generic-метод для ItemFinder

--- QA.DotNetCore.Engine/src/QA.DotNetCore.Engine.Abstractions/Finder/IItemFinder.cs
+++ QA.DotNetCore.Engine/src/QA.DotNetCore.Engine.Abstractions/Finder/IItemFinder.cs
@@ -12,5 +12,14 @@ namespace QA.DotNetCore.Engine.Abstractions.Finder
         /// <param name="depth">максимальная глубина поиска</param>
         /// <returns></returns>
         IAbstractItem Find(IAbstractItem startPage, Func<IAbstractItem, bool> matchCriteria, int depth = 5);
+
+        /// <summary>
+        /// Поиск элементов по условию. Применяется рекурсивный поиск по поддереву начальной страницы
+        /// </summary>
+        /// <param name="startPage">начальная страница для поиска</param>
+        /// <param name="matchCriteria">условие поиска</param>
+        /// <param name="depth">максимальная глубина поиска</param>
+        /// <returns></returns>
+        TAbstractItem Find<TAbstractItem>(IAbstractItem startPage, Func<TAbstractItem, bool> matchCriteria, int depth = 5) where TAbstractItem : IAbstractItem;
     }
 }

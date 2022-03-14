aaeaacfe40a9ce3cb7e64f7904edfc53dee8f7d4
Tigran Khachatrian <KhachatrianTS@quantumart.ru>
2022-03-14 17:16:54 +0300

    #167241 simplify example QA.WidgetPlatform.Api

--- QA.DotNetCore.Engine/src/QA.WidgetPlatform.Api/Controllers/SiteController.cs
+++ QA.DotNetCore.Engine/src/QA.WidgetPlatform.Api/Controllers/SiteController.cs
@@ -35,47 +35,5 @@ namespace QA.WidgetPlatform.Api.Controllers
             [Bind(Prefix = "t")] [FromQuery] CaseInSensitiveDictionary<string> targeting, [FromQuery] string[] fields,
             int? deep)
             => _siteStructureService.Structure(dnsName, targeting, fields, deep);
-
-        /// <summary>
-        /// Получение массива нод, удовлетворяющих переданным фильтрам
-        /// </summary>
-        /// <param name="dnsName">Доменное имя сайта</param>
-        /// <param name="targeting">Словарь значений таргетирования</param>
-        /// <param name="fields">Поля деталей к выдаче. Если пусто, то будут выведены все детали</param>
-        /// <returns></returns>
-        [HttpGet("details")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public IEnumerable<SimpleSiteNodeDetails> Details([Required] [FromQuery] string dnsName,
-            [Bind(Prefix = "t")] [FromQuery] CaseInSensitiveDictionary<string> targeting,
-            [FromQuery] string[] fields)
-            => _siteStructureService.Details(dnsName, targeting, fields);
-
-        /// <summary>
-        /// Получение детальной информации по странице или виджету
-        /// </summary>
-        /// <param name="nodeId">id страницы или виджета</param>
-        /// <returns></returns>
-        [HttpGet("node/{nodeId}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public SiteNodeDetails Node(int nodeId)
-            => _siteStructureService.Node(nodeId);
-
-        /// <summary>
-        /// Получение виджетов для страницы или виджета, сгруппированных по зонам
-        /// </summary>
-        /// <param name="abstractItemId">id страницы или виджета</param>
-        /// <param name="targeting">Словарь значений таргетирования</param>
-        /// <param name="zones">Список виджетных зон (если не передавать, то поиск виджетов не будет производиться для рекурсивных и глобальных зон)</param>
-        /// <returns></returns>
-        [HttpGet("widgets/{abstractItemId}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IDictionary<string, WidgetDetails[]> WidgetsForNode(int abstractItemId,
-            [Bind(Prefix = "t")] [FromQuery] CaseInSensitiveDictionary<string> targeting, [FromQuery] string[] zones)
-            => _siteStructureService.WidgetsForNode(abstractItemId, targeting, zones);
     }
 }
--- QA.DotNetCore.Engine/src/QA.WidgetPlatform.Api/Services/Abstract/ISiteStructureService.cs
+++ QA.DotNetCore.Engine/src/QA.WidgetPlatform.Api/Services/Abstract/ISiteStructureService.cs
@@ -17,33 +17,5 @@ namespace QA.WidgetPlatform.Api.Services.Abstract
         SiteNode Structure(string dnsName,
             IDictionary<string, string> targeting, [FromQuery] string[] fields,
             int? deep);
-
-        /// <summary>
-        /// Получение массива нод, удовлетворяющих переданным фильтрам
-        /// </summary>
-        /// <param name="dnsName">Доменное имя сайта</param>
-        /// <param name="targeting">Словарь значений таргетирования</param>
-        /// <param name="fields">Поля деталей к выдаче. Если пусто, то будут выведены все детали</param>
-        /// <returns></returns>
-        IEnumerable<SimpleSiteNodeDetails> Details(string dnsName,
-            IDictionary<string, string> targeting,
-            string[] fields);
-
-        /// <summary>
-        /// Получение детальной информации по странице или виджету
-        /// </summary>
-        /// <param name="nodeId">id страницы или виджета</param>
-        /// <returns></returns>
-        SiteNodeDetails Node(int nodeId);
-
-        /// <summary>
-        /// Получение виджетов для страницы, сгруппированных по зонам
-        /// </summary>
-        /// <param name="abstractItemId">id страницы или виджета</param>
-        /// <param name="targeting">Словарь значений таргетирования</param>
-        /// <param name="zones">Список виджетных зон (если не передавать, то поиск виджетов не будет производиться для рекурсивных и глобальных зон)</param>
-        /// <returns></returns>
-        IDictionary<string, WidgetDetails[]> WidgetsForNode(int abstractItemId,
-            IDictionary<string, string> targeting, string[] zones);
     }
 }

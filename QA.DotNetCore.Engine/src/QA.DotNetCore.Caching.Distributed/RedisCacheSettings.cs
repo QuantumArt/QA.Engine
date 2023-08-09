a5fd74c1b0e84c132419c9d2d783f2b48822af0e
Pavel Celut <celutp@quantumart.ru>
2023-08-09 18:08:19 +0300

    Merged PR 40671: Fix settings

--- QA.DotNetCore.Engine/src/QA.DotNetCore.Caching.Distributed/RedisCacheSettings.cs
+++ QA.DotNetCore.Engine/src/QA.DotNetCore.Caching.Distributed/RedisCacheSettings.cs
@@ -4,15 +4,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace QA.DotNetCore.Caching.Distributed
 {
-    public class RedisCacheSettings : ExternalCacheSettings, IValidatableObject
+    public class RedisCacheSettings : ExternalCacheSettings
     {
-        private static readonly TimeSpan _minTagExpirationOffset = TimeSpan.FromSeconds(1);
-        private static readonly TimeSpan _minLockExpiration = TimeSpan.FromMilliseconds(100);
         private static readonly TimeSpan _defaultTagExpirationOffset = TimeSpan.FromSeconds(5);
-        private static readonly TimeSpan _defaultDeprecatedCacheTimeToLive = TimeSpan.FromSeconds(20);
-        private static readonly TimeSpan _defaultLockExpiration = TimeSpan.FromSeconds(12);
-        private static readonly TimeSpan _defaultRetryEnterLockInverval = TimeSpan.FromMilliseconds(100);
-
 
         public bool UseCompression { get; set; }
 
@@ -25,43 +19,7 @@ namespace QA.DotNetCore.Caching.Distributed
         /// <summary>
         /// Specifies how much longer will live a tag over its associated key.
         /// </summary>
-        [Required]
         public TimeSpan TagExpirationOffset { get; set; } = _defaultTagExpirationOffset;
 
-
-        /// <summary>
-        /// Time that lock lives if client doesn't extend it (e.g. due to critical failure).
-        /// </summary>
-        public TimeSpan LockExpiration { get; set; } = _defaultLockExpiration;
-
-        /// <summary>
-        /// Interval to try to acquire distributed lock.
-        /// </summary>
-        public TimeSpan RetryEnterLockInverval { get; set; } = _defaultRetryEnterLockInverval;
-
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (TagExpirationOffset < _minTagExpirationOffset)
-            {
-                yield return new ValidationResult(
-                    $"Tag expiration mustn't be less than {_minTagExpirationOffset}.",
-                    new[] {nameof(TagExpirationOffset)});
-            }
-
-
-            if (LockExpiration < _minLockExpiration)
-            {
-                yield return new ValidationResult(
-                    $"Lock expiration mustn't be less than {_minLockExpiration}.",
-                    new[] {nameof(LockExpiration)});
-            }
-
-            if (RetryEnterLockInverval < TimeSpan.Zero)
-            {
-                yield return new ValidationResult(
-                    "Retry lock enter interval mustn't be negative.",
-                    new[] {nameof(RetryEnterLockInverval)});
-            }
-        }
     }
 }
